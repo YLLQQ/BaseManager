f63e124aad17cc44b29096b2cc28d6faf7148782
yangguoqing <ysh1059@163.com>
2020-04-28 14:43:48 +0800

    update frontend

--- frontend/src/pages/welcome.js
+++ frontend/src/pages/welcome.js
@@ -1,6 +1,5 @@
-import welcome from '@/assets/welcome.png';
 export default () => (
-  <div style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', marginLeft: '100px'}}>
-    <img src={welcome} alt="欢迎" />
+  <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', marginLeft: '100px' }}>
+    欢迎
   </div>
 );
